34780b3532ef67dd168b350ed8eea36c670622cb
Sydnee <svwoodhouse@gmail.com>
2016-09-13 01:25:55 -0400

    task 1- 4 completed

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -2,19 +2,57 @@
 * CMPSC 261, Section 1
 * Fall 2016
 * Instructor: Phil O'Connell
-* TODO: DON'T FORGET TO PUT YOUR NAME AND ID
 * Student: Sydnee Woodhouse
 * ID: svw5194
 */
 package wheeloffortune;
 
-public class WheelOfFortune {
+import java.util.Scanner;
 
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    
+public class WheelOfFortune 
+{
+  public static void main(String[] args) 
+  {
+      Scanner keyboard = new Scanner(System.in);
+      Scanner key = new Scanner(System.in);
+    // Present the user with a simple text menu of those choices
+    // spin the wheel, buy a vowel, solve the puzzle, or quit the game
+    // Print back to the screen what choice was made
+    // The choice to quit is the only one having functionality right now
+    // Choosing that will exit the program
+      
+      System.out.println("Wheel Of Fortune");
+      System.out.println("1. Spin the wheel");
+      System.out.println("2. Buy a vowel");
+      System.out.println("3. Solve the puzzle");
+      System.out.println("4. Quit the game");
+      
+      int userChoice;
+     
+      while(true)
+      {
+          userChoice = keyboard.nextInt();
+          
+          if (userChoice == 4)
+          {
+             break;
+          }
+            switch(userChoice)
+            {
+              case 1: System.out.println("You chose to spin the wheel.");
+                      break;
+              case 2: System.out.println("You chose to buy a vowel.");
+                      break;
+              case 3: System.out.println(" You chose to solve the puzzle.");
+                      break;
+            }
+            
+            String userLetter;
+            System.out.println("Enter a letter: ");
+            userLetter = key.nextLine();
+            System.out.println("You entered: " + userLetter);
+      }
+              
   }
   
 }
