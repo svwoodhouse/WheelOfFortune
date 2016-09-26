33d1f8c0ecbc9126df069fccee112efe49f9f409
Sydnee <svwoodhouse@gmail.com>
2016-09-26 07:43:36 -0400

    Fin

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -15,25 +15,32 @@ public class WheelOfFortune
   {
       Scanner keyboard = new Scanner(System.in);
       Scanner key = new Scanner(System.in);
+      WheelClass wheel = new WheelClass();
+      
     // Present the user with a simple text menu of those choices
     // spin the wheel, buy a vowel, solve the puzzle, or quit the game
     // Print back to the screen what choice was made
     // The choice to quit is the only one having functionality right now
     // Choosing that will exit the program
-      
-      System.out.println("Wheel Of Fortune");
-      System.out.println("1. Spin the wheel");
-      System.out.println("2. Buy a vowel");
-      System.out.println("3. Solve the puzzle");
-      System.out.println("4. Quit the game");
-      System.out.println("5. Test");
-      
-      int userChoice;
      
-      while(true)
-      {
-          userChoice = keyboard.nextInt();
+    // Initial User 
+      int userChoice;
+      int count = 0;
+        System.out.println("======================");
+        System.out.println("=  Wheel Of Fortune  =");
+        System.out.println("======================");
+        System.out.println(wheel.phraseConcealed);
+        System.out.println("1. Spin the wheel");
+        System.out.println("2. Buy a vowel");
+        System.out.println("3. Solve the puzzle");
+        System.out.println("4. Quit");
+        System.out.println("8. Toggle puzzle reveal");
+        System.out.println("9. Test letter input");
+        System.out.println("Enter choice: ");
           
+      while(true)
+      {           
+        userChoice = keyboard.nextInt();
           if (userChoice == 4)
           {
              break;
@@ -43,49 +50,21 @@ public class WheelOfFortune
               case 1: System.out.println("You chose to spin the wheel.");
                       break;
               case 2: System.out.println("You chose to buy a vowel.");
+                      wheel.userGuess();
                       break;
               case 3: System.out.println(" You chose to solve the puzzle.");
+                      wheel.userGuess();
                       break;
-              case 5: testInput();
-                        break;
+              case 8: count+=1;
+                      wheel.toggleOption(count);
+                      break;
+              case 9: wheel.testInput();
+                      break;
             }
-            
-            String userLetter;
-            System.out.println("Enter a letter: ");
-            userLetter = key.nextLine();
-            char u = userLetter.charAt(0);
-            
-            while(userLetter.length() > 1 || ((int)u < 65 || (int)u > 122))
-            {
-                System.out.println("Error. Enter a valid letter.");
-                System.out.println("Enter a letter: ");
-                userLetter = key.nextLine();
-                u = userLetter.charAt(0);
-            }
-            
-            System.out.println("You entered: " + userLetter);
       }
               
   }
-  
-  static void testInput()
-  {
-            String userLetter;
-            Scanner key = new Scanner(System.in);
-            
-            System.out.println("Enter a letter: ");
-            userLetter = key.nextLine();
-            char u = userLetter.charAt(0);
-            
-            while(userLetter.length() > 1 || ((int)u < 65 || (int)u > 122))
-            {
-                System.out.println("Error. Enter a valid letter.");
-                System.out.println("Enter a letter: ");
-                userLetter = key.nextLine();
-                u = userLetter.charAt(0);
-            }
-            
-            System.out.println("You entered: " + userLetter);
-  }
+
+
   
 }
