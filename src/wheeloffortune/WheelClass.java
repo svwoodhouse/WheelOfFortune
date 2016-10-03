7ee8638fe375164004f377f46da31807d788fff2
Sydnee <svwoodhouse@gmail.com>
2016-10-03 03:33:48 -0400

    Finished, Just need to add comments

--- src/wheeloffortune/WheelClass.java
+++ src/wheeloffortune/WheelClass.java
@@ -1,43 +1,116 @@
 package wheeloffortune;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class WheelClass
 {
-    // Variables
-    public String phrase = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
-    public String phraseConcealed = phrase.replaceAll("[ABCDEFGHIJKLMNOPQRSTUVWXYZ]", "_ ");
-    public char userGuessPhrase[] =phrase.toCharArray();
+    // Variables to keep track of the user information
+    public int userWinnings = 0;
+    public String phrases[] = {"THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", "SOMEWHERE OVER THE RAINBOW", "PETER PAN","BATMAN"};
     
+    // An arraylist to keep track of the guesses that the user already submitted
+    public ArrayList userGuessLetters = new ArrayList();
+ 
+    // Random option that chooses a puzzle from the array of strings
+    Random r = new Random();
+    int randPhrase = r.nextInt(3);
+    
+    // Shows the phrase 
+    public String initPhraseConcealed = phrases[randPhrase].replaceAll("[ABCDEFGHIJKLMNOPQRSTUVWXYZ]", "_ ");
+    public String endPhraseConcealed = phrases[randPhrase].replaceAll("[ABCDEFGHIJKLMNOPQRSTUVWXYZ]", "_");
+    public int length = phrases[randPhrase].length();
+    public char userGuessPhrase[] = endPhraseConcealed.toCharArray();
+    
+    // This just keeps track of the amount that the user spun
+    public int wedge = 0;
+    
+    
+    // Function for when user spins the wheel
+    // Finds the amount of money the user spun then calls the user guess function so that the user  can guess the letter
     void spinWheel()
     {
-        // Random choses a number between 0 and 1;
-        // Multiply by 24 to find a whole number to compare it with;
-        int r = (int)Math.random() * 24;
-        //double weight = 24;
+        // Random choses a number between 0 and 23;
+        Random rand = new Random();
+        int randWedge = rand.nextInt(23);
         
-        int wedges[] = {300,350,400,450,500,550,600,700,800,900,5000};
-      //  double[] probabilities = {5,1,2,1,3,1,3,1,2,2,1,1,1};
+        int wedges[] = {300,300,300,300,300,350,400,400,450,500,500,500,550,600,600,600,700,700,800,800,900,5000};
         
-        // If random number is one or zero, results in the string outputs
-        if(r == 0)
+        // If random number is one or zero, results in the string outputs and setsthe user winnings to zero
+        if(randWedge == 0)
         {
             System.out.println("You landed on: Lose a turn ");
+            this.userWinnings +=0;
+            this.wedge = 0;
         }
         
-        else if(r==1)
+        else if(randWedge==1)
         {
            System.out.println("You landed on: Bankrupt"); 
+           this.userWinnings = 0;
+           this.wedge = 0;
         }
         
+        // Sets the wedge variable to the number given for future reference
         else
         {
-         System.out.println("You landed on: " + wedges[r]);
+         System.out.println("You landed on: " + wedges[randWedge]);
+         this.wedge = wedges[randWedge];
         }
-      
+        
+        // Calls the user guess function
+        userGuess();    
+    }
+  
+    // Function for user to buy vowels
+void buyVowel()
+{
+    Scanner input = new Scanner(System.in);
+    String vowel;
+    char vowelChar;
+    char phraseArray[] = phrases[randPhrase].toCharArray();
+   
+    //Checks to see if the user has enough money
+    if(this.userWinnings < 250)
+    {
+        System.out.println("Error. You don't have enough money to buy vowels.");
+        menu();
     }
     
+    //  Subtracts the amount of money from the user winnings and compares it to the string to see if its in the phrase
+    else
+    {
+        this.userWinnings -=250;
+        System.out.println("Enter a vowel: ");
+        vowel = input.nextLine();
+        vowelChar = vowel.charAt(0);
+        
+for(int i = 0; i < phraseArray.length; i++)
+     {
+         
+         if(phraseArray[i] == vowelChar)
+         {
+             this.userGuessPhrase[i] = vowelChar;
+         }
+         else if(phraseArray[i] == this.userGuessPhrase[i])
+         {
+             this.userGuessPhrase[i] = phraseArray[i];
+         }
+         else if(phraseArray[i] == ' ')
+         {
+             this.userGuessPhrase[i] = ' ';
+         }
+         else
+             this.userGuessPhrase[i] = '_';
+         
+     }
+     // Calls the menu function
+        menu();
+        
+  }
+}
     // Toggle option class. If integer number is even, displays the string that includes the user guesses.
     // If odd, displays the entire string
 void toggleOption(int i)
@@ -52,8 +125,6 @@ void toggleOption(int i)
         System.out.println("2. Buy a vowel");
         System.out.println("3. Solve the puzzle");
         System.out.println("4. Quit");
-        System.out.println("8. Toggle puzzle reveal");
-        System.out.println("9. Test letter input");
         System.out.println("Enter choice: ");
     }
     
@@ -62,13 +133,11 @@ void toggleOption(int i)
         System.out.println("======================");
         System.out.println("=  Wheel Of Fortune  =");
         System.out.println("======================");
-        System.out.println(phrase);
+        System.out.println(phrases[randPhrase]);
         System.out.println("1. Spin the wheel");
         System.out.println("2. Buy a vowel");
         System.out.println("3. Solve the puzzle");
         System.out.println("4. Quit");
-        System.out.println("8. Toggle puzzle reveal");
-        System.out.println("9. Test letter input");
         System.out.println("Enter choice: ");
     }
 }
@@ -80,13 +149,16 @@ void toggleOption(int i)
       String userLetter;
       System.out.println("Enter a letter: ");
       
-      userLetter = key.next();
+      userLetter = key.nextLine();
       char u = userLetter.charAt(0);
-      char phraseArray[] = phrase.toCharArray();
-      System.out.println(phraseArray);
+      char phraseArray[] = phrases[randPhrase].toCharArray();
            
       // Validation for if the user entered a letter
-      while(userLetter.length() > 1 || ((int)u < 65 || (int)u > 122))
+      // Checks to make sure user does not enter a vowel
+      // Prompts user to re enter a letter
+      // The number values 65 and 122 represents the decimal value in the ASCII Table for the letters A to z
+      // The numbers 65, 69, 73, 79, 85 represent the decimal values for the vowels in the ASCII table
+      while(userLetter.length() > 1 || ((int)u < 65 || (int)u > 122 || (int)u == 65 || (int) u == 69 || (int) u == 73 || (int) u == 79 || (int) u == 85))
         {
             System.out.println("Error. Enter a valid letter.");
             System.out.println("Enter a letter: ");
@@ -94,11 +166,26 @@ void toggleOption(int i)
             u = userLetter.charAt(0);
         }
       
+      while(this.userGuessLetters.contains(u))
+      {
+            System.out.println("Error. Already entered that letter.");
+            System.out.println("Enter a different letter: ");
+            userLetter = key.nextLine();
+            u = userLetter.charAt(0);
+      }
+      this.userGuessLetters.add(u);
+      
      for(int i = 0; i < phraseArray.length; i++)
      {
+         
          if(phraseArray[i] == u)
          {
              this.userGuessPhrase[i] = u;
+             this.userWinnings += this.wedge;
+         }
+         else if(phraseArray[i] == this.userGuessPhrase[i])
+         {
+             this.userGuessPhrase[i] = phraseArray[i];
          }
          else if(phraseArray[i] == ' ')
          {
@@ -106,18 +193,9 @@ void toggleOption(int i)
          }
          else
              this.userGuessPhrase[i] = '_';
+         
      }
-        System.out.println("======================");
-        System.out.println("=  Wheel Of Fortune  =");
-        System.out.println("======================");
-        System.out.println(this.userGuessPhrase);
-        System.out.println("1. Spin the wheel");
-        System.out.println("2. Buy a vowel");
-        System.out.println("3. Solve the puzzle");
-        System.out.println("4. Quit");
-        System.out.println("8. Toggle puzzle reveal");
-        System.out.println("9. Test letter input");
-        System.out.println("Enter choice: ");
+        menu();
       }
     
     
@@ -142,4 +220,69 @@ void toggleOption(int i)
             
             System.out.println("You entered: " + userLetter);
   }
+  
+void menu()
+{
+        System.out.println("======================");
+        System.out.println("=  Wheel Of Fortune  =");
+        System.out.println("======================");
+        System.out.println(this.userGuessPhrase);
+        System.out.println("$" + this.userWinnings);
+        System.out.println("1. Spin the wheel");
+        System.out.println("2. Buy a vowel");
+        System.out.println("3. Solve the puzzle");
+        System.out.println("4. Quit");
+        System.out.println("Enter choice: ");
+}
+
+
+void solvePuzzle()
+{
+      Scanner key = new Scanner(System.in);
+      String userLetter;
+      boolean run = true;
+      
+while(run)
+{
+      System.out.println("Enter a letter: ");
+
+      userLetter = key.nextLine();
+      char u = userLetter.charAt(0);
+      char phraseArray[] = phrases[randPhrase].toCharArray();
+      
+if(phrases[randPhrase].contains(userLetter))
+    {  
+     for(int i = 0; i < phraseArray.length; i++)
+     {       
+        if(phraseArray[i] == u)
+        {
+             this.userGuessPhrase[i] = u;
+             this.userWinnings += this.wedge;
+             
+            if(phraseArray[i] == this.userGuessPhrase[i])
+            {
+                this.userGuessPhrase[i] = phraseArray[i];
+            }
+             else if(phraseArray[i] == ' ')
+            {
+                this.userGuessPhrase[i] = ' ';
+            }
+            else
+                this.userGuessPhrase[i] = '_';  
+        }
+        
+        if (this.userGuessPhrase.toString().equalsIgnoreCase(this.phrases[randPhrase]))
+        {
+            System.out.println("You win!");
+            break;
+        }
+     }
+    }
+else
+{
+    System.out.println("You lose!");
+    break;
+}
+}
+}
 }
